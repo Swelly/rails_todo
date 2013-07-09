24643b0a26117a6b63b65848b77609a8e00ca1f4
Wesley Carr <surfswell17@gmail.com>
2013-07-08 22:15:15 -0400

    created table :tasks

--- Rails_ToDo/db/migrate/20130709020900_create_tasks_to_do.rb
+++ Rails_ToDo/db/migrate/20130709020900_create_tasks_to_do.rb
@@ -1,8 +1,14 @@
 class CreateTasksToDo < ActiveRecord::Migration
   def up
-
+    create_table :tasks do |t|
+      t.string :todo
+      t.string :details
+      t.datetime :due
+      t.boolean :status
+    end
   end
 
   def down
+    drop_table :tasks
   end
 end
