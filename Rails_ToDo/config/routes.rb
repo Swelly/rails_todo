615ccf63f6f196d242a29416928d6052ca964bef
Wesley Carr <surfswell17@gmail.com>
2013-07-08 22:41:57 -0400

    Updating controllers

--- Rails_ToDo/config/routes.rb
+++ Rails_ToDo/config/routes.rb
@@ -1,3 +1,14 @@
 RailsTodo::Application.routes.draw do
+  # Missed this in class... attempting to make it work
   root :to => 'welcome#index'
+  resources :tasks do
+    collection do
+      get 'search'
+    end
+
+    member do
+      get 'tasks'
+    end
+  end
+
 end
